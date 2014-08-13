40ae37667a85d5db5650be20b64855758afbef18
Jim Brower <jimrabrower@gmail.com>
2014-08-13 11:46:09 -0700

    finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,4 @@
+
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -33,4 +34,7 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering bcrypt's cost function.
+  ActiveModel::SecurePassword.min_cost = true
 end
